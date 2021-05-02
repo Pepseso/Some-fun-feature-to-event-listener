6511d150b4cea64bde481469464e809079dcb9ea
Pepseso <juna.josef@gmail.com>
2021-05-02 22:20:11 +0200

    j

---
+++ script.js
@@ -0,0 +1,9 @@
+$('.first').on('click', () => {
+    console.log("Yeah, you clicked me")
+  });
+$('.second').on('click', () => {
+    $(".first").css("background-color", "blue")
+  });
+  $('.third').on('click', () => {
+    $("button").css("background-color", val(.barva))
+});
